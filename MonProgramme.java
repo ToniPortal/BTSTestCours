c9b237a492c354076257bef55d9f9f3aa62f9c1e
unknown <PASTRE@SLAM21-01.slam.sio>
2021-09-07 09:50:53 +0200

    first commit

---
+++ MonProgramme.java
@@ -0,0 +1,25 @@
+public class MonProgramme {
+
+
+    public static void main(String[] args){
+    
+int var1 = 25;
+int var2 = 10;
+
+var1 = var1 - 10;
+
+int ajout = var1 + var2;
+
+        System.out.println("var :"+ var1 + "+"+ var2);
+        System.out.println("Ajout :"+ ajout);
+
+String nom = "toto";
+String prenom = "pa";
+String telephone = "061020304050";
+String adresse = "rue des champs";
+
+System.out.println(nom + "\n" + prenom + "\n" + telephone + "\n" + adresse + "\n");
+
+    }
+
+}

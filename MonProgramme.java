8ee1fd852768dc12422ff9270fe9f04282dd8e6c
PASTRE <tonitoctoc13@gmail.com>
2021-09-07 09:58:20 +0200

    test1

--- MonProgramme.java
+++ MonProgramme.java
@@ -1,24 +1,23 @@
 public class MonProgramme {
 
+    public static void main(String[] args) {
 
-    public static void main(String[] args){
-    
-int var1 = 25;
-int var2 = 10;
+        int var1 = 25;
+        int var2 = 10;
 
-var1 = var1 - 10;
+        var1 = var1 - 10;
 
-int ajout = var1 + var2;
+        int ajout = var1 + var2;
 
-        System.out.println("var :"+ var1 + "+"+ var2);
-        System.out.println("Ajout :"+ ajout);
+        System.out.println("var :" + var1 + "+" + var2);
+        System.out.println("Ajout :" + ajout);
 
-String nom = "toto";
-String prenom = "pa";
-String telephone = "061020304050";
-String adresse = "rue des champs";
+        String nom = "toto";
+        String prenom = "pa";
+        String telephone = "061020304050";
+        String adresse = "rue des champs";
 
-System.out.println(nom + "\n" + prenom + "\n" + telephone + "\n" + adresse + "\n");
+        System.out.println(nom + "\n" + prenom + "\n" + telephone + "\n" + adresse + "\n");
 
     }
 

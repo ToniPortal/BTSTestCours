9521521802da6862ee3c5375d9c1030a8173a2b4
PASTRE <tonitoctoc13@gmail.com>
2021-09-07 10:46:13 +0200

    test2

--- MonProgramme.java
+++ MonProgramme.java
@@ -9,15 +9,36 @@ public class MonProgramme {
 
         int ajout = var1 + var2;
 
+int maxrandom = 50 + 1;
+
+int divmaxrand = maxrandom / 2;
+
+        int random = (int)(Math.random() * maxrandom); 
+
+System.out.println("random;" + random);
+
         System.out.println("var :" + var1 + "+" + var2);
         System.out.println("Ajout :" + ajout);
 
-        String nom = "toto";
-        String prenom = "pa";
+        String nom = "pas";
+        String prenom = "toto";
         String telephone = "061020304050";
         String adresse = "rue des champs";
 
         System.out.println(nom + "\n" + prenom + "\n" + telephone + "\n" + adresse + "\n");
+
+
+if(random >= divmaxrand){
+
+    System.out.println("plus ou égal a " + divmaxrand);
+
+}else {
+
+    System.out.println("en dessoue de " + divmaxrand);
+
+}
+
+
 
     }
 

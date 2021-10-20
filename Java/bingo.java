96d527ad621a01576483078c3f7f263195d21467
PASTRE <tonitoctoc13@gmail.com>
2021-10-20 12:48:03 +0200

    commit

--- Java/bingo.java
+++ Java/bingo.java
@@ -3,31 +3,28 @@ package Java;
 import java.util.Scanner;
 
 public class bingo {
-
     public static void main(String[] args) {
 
         System.out
-                .println("Choisie 1 : mettre un nombre random de nombre a sortir\nChoisie 2 ; pour sortir 10 chiffre");
+                .println("Choisie 1 : mettre un nombre random de nombre a sortir\nChoisie 2 ; pour sortir 75 chiffre");
 
         Scanner MonScanneur = new Scanner(System.in);
-        int ouioupas = MonScanneur.nextInt();
-        int nombreasortir = 10;
-        if (ouioupas == 2) {
-            nombreasortir = 10;
-        } else if (ouioupas == 1) {
+        int ouioupas = MonScanneur.nextInt(); // Selection d'un nombre random un du chiffre
+        int nombreasortir = 75;
 
-            nombreasortir = MonScanneur.nextInt();
+        if (ouioupas == 1) {
+
+            nombreasortir = MonScanneur.nextInt(); // Nombre a sortir pour le bingo
 
         }
 
-        int[] j1 = new int[nombreasortir];
-        int[] j2 = new int[nombreasortir];
-        int[] bingonumber = new int[nombreasortir];
+        int[] j1 = new int[nombreasortir]; // Tableau j1
+        int[] j2 = new int[nombreasortir]; // Tableau j2
+        int[] bingonumber = new int[nombreasortir]; // tableau qui sortiron
 
-        int boucle = 0;
         int min = 1;
         int max = 100;
-
+        int boucle = 0;
         while (boucle < nombreasortir) { // alimenter j1,j2,bingonumber
             j1[boucle] = min + (int) (Math.random() * ((max - min) + min));
             j2[boucle] = min + (int) (Math.random() * ((max - min) + min));
@@ -36,22 +33,32 @@ public class bingo {
             boucle++;
         }
 
+
+
+        
+
         int boucle2 = 0;
         int testchiffre = 0;
 
+        int nombrebouclebingo = 0;
         while (boucle2 < 1) {
 
-            if (bingonumber[testchiffre] == j1[testchiffre] || bingonumber[testchiffre] == j2[testchiffre]) {
+            if (bingonumber[testchiffre] == j1[testchiffre]) {
 
                 boucle2++;
-                System.out.println("Win");
+                System.out.println("Win J1\nVous avez du relancer:" + nombrebouclebingo + " fois pour gagnez");
+
+            } else if (bingonumber[testchiffre] == j2[testchiffre]) {
+
+                boucle2++;
+                System.out.println("Win J2\nVous avez du relancer:" + nombrebouclebingo + " fois pour gagnez");
 
             } else {
                 testchiffre++;
 
                 if (testchiffre == nombreasortir) {
-
                     for (int boucle3 = 0; boucle3 < nombreasortir; boucle3++) {
+                        nombrebouclebingo++;
                         testchiffre = 0;
                         bingonumber[boucle3] = min + (int) (Math.random() * ((max - min) + min));
                         System.out.println("New bingo Number: " + bingonumber[boucle3]);
---
+++ Java/testsuplicate.java
@@ -0,0 +1,24 @@
+package Java;
+
+public class testsuplicate {
+
+    public static void main(String[] args) {
+
+        int[] tableau = { 10, 10, 50, 84, 15, 89, 84, 450 };
+
+        int boucle = 1;
+
+        int numberapres = 0;
+        int min = 1;
+        int max = 100;
+        while (tableau[numberapres] == tableau[boucle]) {
+            tableau[boucle] = min + (int) (Math.random() * ((max - min) + min));
+            numberapres++;
+            boucle++;
+        }
+
+        for (int boucle1 = 0; boucle1 < 8; boucle1++) {
+            System.out.println(tableau[boucle1]);
+        }
+    }
+}
